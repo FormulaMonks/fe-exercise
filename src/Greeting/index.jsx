392adec6cf14e72fcf628933a0a7c6f4d2d49d01
Denis Sokolov <denis@sokolov.cc>
2021-03-01 12:00:00 +0000

    Greeting: sample logo

--- src/Greeting/index.jsx
+++ src/Greeting/index.jsx
@@ -1,5 +1,11 @@
 import styles from "./styles.css";
+import logo from "./logo.svg";
 
 export function Greeting() {
-  return <div className={styles.container}>Hello!</div>;
+  return (
+    <div className={styles.container}>
+      <img alt="Theorem logo" className={styles.logo} src={logo} width="100" />
+      <div>Hello!</div>
+    </div>
+  );
 }
