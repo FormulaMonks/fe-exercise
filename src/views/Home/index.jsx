f38babae618bff16398ff36c068a64b08fad2cc3
Denis Sokolov <denis@sokolov.cc>
2021-03-09 12:00:00 +0000

    views/Home: show a list of people to give feedback to

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,11 +1,17 @@
 import styles from "./styles.css";
 import { MainLayout } from "src/layouts";
+import { usePeople } from "src/data";
+import { Line } from "./Line";
 
 export function Home() {
+  const people = usePeople();
   return (
     <MainLayout title="Home | Honesto">
-      <div className={styles.container}>
-        <div>Hello!</div>
+      <h1>Share Feedback</h1>
+      <div className={styles.card}>
+        {people.map((person) => (
+          <Line key={person.id} person={person} />
+        ))}
       </div>
     </MainLayout>
   );
