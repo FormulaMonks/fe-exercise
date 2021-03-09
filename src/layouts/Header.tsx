05618fb1e99af361ef4d0ab24e7a845421b8a219
Denis Sokolov <denis@sokolov.cc>
2021-03-09 12:00:00 +0000

    layouts/Header: minor styling

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,6 +4,7 @@ const Root = styled.div`
   align-items: center;
   background-color: #201d29;
   box-shadow: 0px 0px 4px rgb(0 0 0 / 25%);
+  box-sizing: border-box;
   color: white;
   display: flex;
   min-height: 76px;
