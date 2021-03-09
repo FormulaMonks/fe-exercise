de1092c0827fbd93998875e5ac892ba591f9fabe
Denis Sokolov <denis@sokolov.cc>
2021-03-09 12:00:00 +0000

    links: link to the current page is not clickable

--- src/links/index.tsx
+++ src/links/index.tsx
@@ -1,9 +1,16 @@
 import { AnchorHTMLAttributes } from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const RoutingLink = styled(NavLink)`
+  &.active {
+    pointer-events: none;
+  }
+`;
 
 export function Link(props: AnchorHTMLAttributes<HTMLAnchorElement>) {
   const { href } = props;
   if (href === "/" || href?.match(/^\/[^/]/))
-    return <NavLink {...props} to={href} />;
+    return <RoutingLink {...props} exact to={href} />;
   return <a {...props} />;
 }
