89dd72f5026d2df001d548d41b1fb6dfc62f3876
Denis Sokolov <denis@sokolov.cc>
2021-03-11 12:00:00 +0000

    views/NotFound: style

--- src/views/NotFound/index.tsx
+++ src/views/NotFound/index.tsx
@@ -1,13 +1,33 @@
+import styled from "styled-components";
+import { Card } from "src/Card";
 import { MainLayout } from "src/layouts";
+
+const H1 = styled.h1`
+  font-size: 28px;
+
+  margin: 0 0 40px;
+`;
+
+const Message = styled.div`
+  font-size: 18px;
+  line-height: 1.4;
+  padding: 40px;
+`;
+
+const P = styled.p`
+  margin: 0;
+`;
 
 export function NotFound() {
   return (
     <MainLayout title="Not found">
-      <h1>URL problem</h1>
-      <p>
-        Sorry, the URL may be incorrect. Check the spelling or navigate using
-        the menu above.
-      </p>
+      <Card>
+        <Message>
+          <H1>Page not found</H1>
+          <P>Sorry, the URL you have used may be incorrect.</P>
+          <P>Check the spelling or navigate using the menu above.</P>
+        </Message>
+      </Card>
     </MainLayout>
   );
 }
