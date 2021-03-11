d0a7a1faba981c018a6a49de6fa5981f8035a896
Denis Sokolov <denis@sokolov.cc>
2021-03-11 12:00:00 +0000

    layouts/Header: nav links

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -3,15 +3,21 @@ import { accentColor } from "src/colors";
 import { Link } from "src/links";
 
 const Root = styled.div`
-  align-items: center;
+  align-items: stretch;
   background-color: ${accentColor};
   box-shadow: 0px 0px 4px rgb(0 0 0 / 25%);
   box-sizing: border-box;
   color: white;
   display: flex;
+  gap: 10px;
   min-height: 76px;
   min-width: 100%;
   padding: 0 32px;
+  & > * {
+    align-items: center;
+    display: flex;
+    padding: 0 15px;
+  }
 `;
 
 const Logo = styled(Link)`
@@ -21,10 +27,19 @@ const Logo = styled(Link)`
   text-decoration: none;
 `;
 
+const NavLink = styled(Link)`
+  color: white;
+  font-size: 18px;
+  font-weight: bold;
+  text-decoration: none;
+`;
+
 export function Header() {
   return (
     <Root>
       <Logo href="/">Honesto</Logo>
+      <NavLink href="/">Give feedback</NavLink>
+      <NavLink href="/review">Review feedback</NavLink>
     </Root>
   );
 }
