57aba67b453de2ba4d828721bff6f2a63203d774
Denis Sokolov <denis@sokolov.cc>
2021-03-12 12:00:00 +0000

    views/Home: support loading states

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -2,10 +2,12 @@ import styles from "./styles.css";
 import { Card } from "src/Card";
 import { MainLayout } from "src/layouts";
 import { usePeople } from "src/data";
+import { Loading } from "../Loading";
 import { Line } from "./Line";
 
 export function Home() {
   const people = usePeople();
+  if (people === "loading") return <Loading />;
   return (
     <MainLayout title="Home | Honesto">
       <h1 className={styles.heading}>Give Feedback</h1>
