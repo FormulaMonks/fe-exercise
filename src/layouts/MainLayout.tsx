49377a7b478304e27e28f5eaa9ee3f6e1ae12fcd
Denis Sokolov <denis@sokolov.cc>
2021-03-12 12:00:00 +0000

    layouts/Footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,19 +1,24 @@
 import { ReactNode, useEffect } from "react";
 import styled from "styled-components";
 import prism from "./prism.svg";
+import { Footer } from "./Footer";
 import { Header } from "./Header";
 
 const Background = styled.div`
   /*! background by SVGBackgrounds.com */
   background: white url("${prism}") no-repeat center center;
   min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Content = styled.div`
   box-sizing: border-box;
+  flex: 1;
   max-width: 900px;
   margin: 0 auto;
   padding: 80px 16px 100px;
+  width: 100%;
 `;
 
 type Props = {
@@ -31,6 +36,7 @@ export function MainLayout(props: Props) {
     <Background>
       <Header />
       <Content>{children}</Content>
+      <Footer />
     </Background>
   );
 }
